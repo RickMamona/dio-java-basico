db56d3691cbe2d3c8f698c0c6d7ee35279b7b126
Rick Mamona <rickmamona07@gmail.com>
2024-06-08 12:34:58 -0300

    Acertando numero

--- primeiro-projeto-java/src/edu/rick/anatomiaclasses/Operadores.java
+++ primeiro-projeto-java/src/edu/rick/anatomiaclasses/Operadores.java
@@ -1,20 +1,27 @@
 package edu.rick.anatomiaclasses;
 
+import java.util.Scanner;
+
+
 public class Operadores {
 
     public static void main(String[] args) {
-   boolean condicao1=true;
-   boolean condicao2=false;
+        Scanner in = new Scanner(System.in);
+        int numero = -1;
 
-   if (condicao1 && condicao2) {
+        while (numero !=10) {
 
-    System.out.println("As duas condições são verdadeiras");
-   }
-
-   if (condicao1 || condicao2) {
-    System.out.println("Uma das condições são verdadeiras");
-   }
-   System.out.println("Fim");
+            System.out.println("Digite um numero: ");
+            numero = in.nextInt();
+            if (numero ==10) {
+                System.out.println("Voce acertou!");              
+            }
+            if (numero>10) {
+                System.out.println("É um número menor.");               
+            }
+            if (numero<10){
+                System.out.println("É um número maior.");
+            }          
+        }
 }
-
 }
