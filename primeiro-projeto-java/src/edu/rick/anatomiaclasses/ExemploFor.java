0bacf283a7f49a7761f80d5916e68ae8b5b70551
Rick Mamona <rickmamona07@gmail.com>
2024-06-12 11:10:28 -0300

    exemplofor

---
+++ primeiro-projeto-java/src/edu/rick/anatomiaclasses/ExemploFor.java
@@ -0,0 +1,16 @@
+package edu.rick.anatomiaclasses;
+
+
+public class ExemploFor  {
+
+    public static void main(String[] args) {
+
+        for ( int ovelha = 1; ovelha <=20; ovelha ++){
+
+            System.out.println("Contando Ovelhas " + ovelha);
+        }
+
+        System.out.println("Joaozinho dormiu.");
+        
+    }
+}
