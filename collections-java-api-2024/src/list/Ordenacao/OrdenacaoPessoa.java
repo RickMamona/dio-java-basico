c639fc01f97023e5af1def22c4085b19ee789400
Rick Mamona <rickmamona07@gmail.com>
2024-07-02 12:50:02 -0300

    Ordenacao Lista

---
+++ collections-java-api-2024/src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,44 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+
+    private List<Pessoa> pessoalList;
+
+    public OrdenacaoPessoa() {
+        this.pessoalList = new ArrayList<>();
+    }
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoalList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoalList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoalList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.90);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 16, 1.56);
+
+        System.out.println(ordenacaoPessoa.pessoalList);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+
+
+    }
+}
