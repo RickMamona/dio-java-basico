bc7963f7d5c894e72f2508fa075a28e8bd5a5e6d
Rick Mamona <rickmamona07@gmail.com>
2024-07-02 11:00:26 -0300

    lista tarefa

--- collections-java-api-2024/src/App.java
+++ collections-java-api-2024/src/App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        for(int i = 1; i < 10; i++) {
-
+        for(int i = 1; i < 10; i++) {     
             System.out.println("i = " + i);
     }
  }
