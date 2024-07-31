92fd33f4525b2109a029d9e2e15e3cd6efedb624
Rick Mamona <rickmamona07@gmail.com>
2024-07-31 10:04:42 -0300

    Spring Properties

--- spring-properties-values/src/main/java/dio/spring_properties_values/app/SistemaMensagem.java
+++ spring-properties-values/src/main/java/dio/spring_properties_values/app/SistemaMensagem.java
@@ -1,27 +1,19 @@
 package dio.spring_properties_values.app;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-    @Value("${nome}")
-    private String nome;
-    @Value("${email}")
-    private String email ;
-    @Value("${telefones}")
-    private List<Long> telefones ;
+    @Autowired
+    private Rementente rementente;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome 
-        + "\nE-mail: " + email 
-        + "\nTelefones: " + telefones);
+        System.out.println("Mensagem enviada por: " + rementente.getNome() 
+        + "\nE-mail: " + rementente.getEmail()
+        + "\nTelefones: " + rementente.getTelefones());
         System.out.println("Seu cadastro foi aprovado");
     }
 
